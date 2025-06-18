3b52b9bed20022001da78f99772a9c4e11859c2c
luisruizfdez <luis.ruizfdez@gmail.com>
2025-06-18 11:19:26 -0600

    hola

---
+++ src/main/java/frc/robot/cmd_Move_Motor1.java
@@ -0,0 +1,5 @@
+package frc.robot;
+
+public class cmd_Move_Motor1 {
+
+}
---
+++ src/main/java/frc/robot/commands/Cmd_Move_Motor_1.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Sub_Motores;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class Cmd_Move_Motor_1 extends Command {
+  /** Creates a new Cmd_Move_Motor_1. */
+  private final Sub_Motores Motores;
+  private final Supplier<Double> getLeftXSupplier;
+  public Cmd_Move_Motor_1(Sub_Motores motores, Supplier<Double> getLeftXSupplier) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.Motores=motores;
+    this.getLeftXSupplier = getLeftXSupplier;
+    addRequirements(Motores);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+
+    double leftXValue = getLeftXSupplier.get();
+    System.out.println(leftXValue);
+    Motores.setMotor1Speed(leftXValue);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
